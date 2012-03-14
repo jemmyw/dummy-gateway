31b574da50238fcb9a1c6c9d22b7824bdb9ee338
Jeremy Wells <jemmyw@gmail.com>
2012-03-14 22:28:32 +1300

    initial commit

---
+++ dummy_sms.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require 'couchrest'
+
+module JavascriptHelper
+  def escape_javascript(html_content)
+    return '' unless html_content
+    javascript_mapping = { '\\' => '\\\\', '</' => '<\/', "\r\n" => '\n', "\n" => '\n' }
+    javascript_mapping.merge("\r" => '\n', '"' => '\\"', "'" => "\\'")
+    escaped_string = html_content.gsub(/(\\|<\/|\r\n|[\n\r"'])/) { javascript_mapping[$1] }
+    "\"#{escaped_string}\""
+  end
+end
+
+use Rack::Auth::Basic do |username, password|
+  username == 'api' && password == 'zwUx8HAk4NmfhaheqQ'
+end
+
+before do
+  @db = CouchRest.database!("http://127.0.0.1:5984/dummy_sms")
+end
+
+helpers do
+  include JavascriptHelper
+end
+
+post '/send_sms' do
+  begin
+    response = @db.save_doc({:number => params[:number], :message => params[:message]})
+    @id = response["id"]
+    erb :send_sms
+  rescue => @error
+    erb :error
+  end
+end
+
+get '/send_sms' do
+  erb :send_sms_instructions
+end
---
+++ send_sms.php
@@ -0,0 +1,5 @@
+<?php
+$api_key = "zwUx8HAk4NmfhaheqQ";
+
+if(
+?>
